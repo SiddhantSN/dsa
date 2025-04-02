e19f57d252a8ddc5680286c58f0ed84e4b8aef9d
Siddhant <sid5878@gmail.com>
2025-04-03 01:08:28 +0530

    Arrays 1 update

--- DSA/src/basics/arrays/Arrays1.java
+++ DSA/src/basics/arrays/Arrays1.java
@@ -1,21 +1,41 @@
 package basics.arrays;
 
+import java.util.HashMap;
+
 public class Arrays1 {
 	
-	public static int getConsecutiveOnes(int [] arr) {
-		int n = 0;
-		int v = 0;
-		for (int i : arr) {
-			if(i==1) {
-				n++;
+	public static int getConsecutiveOnes(int [] nums) {
+		int cnt = 0;
+    int maxi = 0;
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) {
+            cnt++;
+        } else {
+            cnt = 0;
+        }
+
+        maxi = Math.max(maxi, cnt);
+    }
+    return maxi;
+    }
+	
+	public static int appearsOnce(int [] nums) {
+		HashMap<Integer, Integer> count = new HashMap<>();
+		Integer find = -1;
+		for (int i : nums) {
+			if(count.containsKey(i)) {
+				count.put(i, count.get(i)+1);
 			}
 			else {
-				if(v<n) {
-					v=n;
-					n=0;
-				}
+				count.put(i, 1);
 			}
 		}
-		return v;
+		for(Integer i : count.keySet()) {
+			if(count.get(i)==1) {
+				find = i;
+				return find;
+			}
+		}
+		return find;
 	}
 }
--- DSA/src/test/Test.java
+++ DSA/src/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		//
-		int [] arr = {1,1,0,1,1,1,0,0,0};
+		int [] arr = {1,1,0,1,1,1};
 		System.out.println(Arrays1.getConsecutiveOnes(arr));
 	}
 }
